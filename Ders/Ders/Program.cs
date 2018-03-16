7b0f89200f9121c5853f4a960c67a6e4ec5e5a66
SoltanovElnur <37265954+SoltanovElnur@users.noreply.github.com>
2018-03-16 16:01:32 +0400

    Tapshiriq

---
+++ Ders/Ders/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Tapshiriq 1.Ekrandan girilen ededin nece reqemli oldugunu ekrana yazdiran proqram yazin
+
+            //int eded;
+            //int sayi = 1;
+            //Console.Write("Bir Eded Girin : ");
+            //eded = Convert.ToInt32(Console.ReadLine());
+            //while (eded >= 9)
+            //{
+            //    eded = eded / 10;
+            //    sayi++;
+            //}
+            //Console.WriteLine(sayi + " - " +"Reqemli bir ededdir");
+
+
+            //----------------------------------------------------------------------------------------------
+
+
+            ////Tapshiriq 2.Ekrandan girilen ededin reqemleri cemini ekrana yazdiran program yazin
+
+            //Console.Write("Iki reqemli bir eded girin : ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int y = 0;
+            //while (x != 0)
+            //{
+            //    y = y + x % 10;
+            //    x = x / 10;
+            //}
+            //Console.WriteLine("Girilen ededin reqemlerinin cemi : " + y);
+
+
+
+            //-----------------------------------------------------------------------------------------------
+
+            //Tapshiriq 3.Ekrandan daxil olunan ifadenin daxilinde sait ve samitleri ayri ayri ekrana yazdiran program yazin
+            //string a;
+            //int sait = 0, metin = 1, samit = 0;
+            //Console.Write("Bir cumle daxil edin : ");
+            //a = Console.ReadLine();
+            //int c = a.Length;
+            //foreach (char b in a)
+            //{
+            //    if (b == 'a' || b == 'ә' || b == 'e' || b == 'ı' || b == 'i' || b == 'o' || b == 'ö' || b == 'u' || b == 'ü' || b == 'A' || b == 'E' || b == 'U' || b == 'I' || b == 'O' || b == 'Ә' || b == 'Ü' || b == 'Ö' || b == 'İ')
+            //        sait++;
+
+            //    else if (b != ' ')
+            //        samit++;
+            //    if (b == ' ')
+            //        metin++;
+            //}
+            //Console.WriteLine(sait + " sait var.");
+            //Console.WriteLine(samit + " samit var.");
+            //Console.WriteLine(metin + " soz var.");
+            //Console.ReadKey();
+
+
+            //-----------------------------------------------------------------------------------------------
+
+            //Tapshiriq 4.Ekrandan daxil olunan cumlenin sozlerini tek tek ekrana cixaran program yazin
+            //{
+            //    string cumle;
+            //    string[]  s;
+            //    Console.Write("Cumleni yazin : ");
+            //    cumle = Console.ReadLine();
+            //    s = cumle.Split(' ');
+            //    Console.WriteLine("*****************************");
+            //    foreach (string soz in s)
+            //    {
+            //        Console.WriteLine(soz);
+            //    }
+            //    Console.ReadKey();
+            //}
+
+
+            //-----------------------------------------------------------------------------------------------
+
+            ////Tapshiriq 5. Menim keywords adinda bir listim var
+            ////Ekrandan daxil olunan ifadede bu sozlerden her hansi biri kecirse ekranda "Siz losersiniz " deye error vermesini isteyirem.
+            //List<string> keywords = new List<string>() { "Tenbel", "Kod yazmayan", "Yola Veren", "Esneyen", "Bekar" };
+            //Console.WriteLine("Bir Ifade daxil edin : ");
+            //string m = Console.ReadLine();
+
+            //        if (g == "Tenbel")
+            //        {
+            //            Console.WriteLine("ERROR : Siz losersiniz");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine(m);
+            //        }
+
+            
+            //  //BERABER ELEYIB YAZDIRA BILMIREM. MEN OZUM ERROR VERDIM JUE
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
---
+++ Ders/Ders/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Ders")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Ders")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d9b305f6-de11-456c-8ee4-add0f3307984")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
